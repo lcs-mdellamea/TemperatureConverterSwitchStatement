78f34b59fd758ed135a9a552d4dbd3865b49f0ac
Madison Dellamea <mdellamea@lcs.on.ca>
2021-12-03 11:09:14 -0500

    Initial Commit

---
+++ TemperatureConverterSwitchStatement/TemperatureConverterSwitchStatementApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemperatureConverterSwitchStatementApp.swift
+//  TemperatureConverterSwitchStatement
+//
+//  Created by Madison Dellamea on 12/3/21.
+//
+
+import SwiftUI
+
+@main
+struct TemperatureConverterSwitchStatementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
